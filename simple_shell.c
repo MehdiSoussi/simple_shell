b88813f51dc1c0e00379b2bd68b5e8e6cb3e044c
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-12-08 06:21:11 -0800

    error fix

--- simple_shell.c
+++ simple_shell.c
@@ -1,27 +1,29 @@
 #include "main.h"
 
-void main (void)
+int main (void)
 {
 	char *buffer = 0;
 	size_t buffer_size = 32;
-	ssize_t return_value;
 	char *argv[] = {".", 0};
-	int res;
 	int x;
 
 	while(1)
 	{
 	x = fork();
 	if(x != 0)
+	{
 		wait(0);
+	return(0);
+	}
 	else
 	{
 		buffer = malloc(sizeof(char)*buffer_size);
 		printf("$ ");
-		return_value = getline(&buffer, &buffer_size, stdin);
+		getline(&buffer, &buffer_size, stdin);
 		printf("\n");
 		execve(buffer, argv, 0 );
 		
 	}
 	}
+	return(0);
 }
