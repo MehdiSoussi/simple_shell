f053549473b6e7440909abacd036d29238c2f83d
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-12-08 05:34:41 -0800

    setting the first project files

---
+++ simple_shell.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void main (void)
+{
+	char *buffer = 0;
+	size_t buffer_size = 32;
+	ssize_t return_value;
+	char *argv[] = {".", 0};
+	int res;
+	int x;
+
+	while(1)
+	{
+	x = fork();
+	if(x != 0)
+		wait(0);
+	else
+	{
+		buffer = malloc(sizeof(char)*buffer_size);
+		printf("$ ");
+		return_value = getline(&buffer, &buffer_size, stdin);
+		printf("\n");
+		execve(buffer, argv, 0 );
+		
+	}
+	}
+}
