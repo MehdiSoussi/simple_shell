477a8c2e49c58385441f9888f460f4d4bc71e19d
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2023-12-10 03:45:29 -0800

    task 2 attempt

--- simple_shell.c
+++ simple_shell.c
@@ -1,56 +1,58 @@
 #include "main.h"
-#include <errno.h>
 
 int main (__attribute__((unused)) int argc, __attribute__((unused)) char **argv, __attribute__((unused)) char **envp)
 {
 
 	char *buffer = 0;
 	size_t buffer_size = 0;
-	int x, r, w;
-	char *token;
+	int x, r, w, i = 0;
+	char **arguments;
 
-		while(1)
+	while(1)
+	{
+		if(isatty(0))
+			printf("$ ");
+		if (getline(&buffer, &buffer_size, stdin) == -1)
+			exit(0);
+
+
+		x = fork();
+		if (x == -1)
 		{
-			if(isatty(0))
-				printf("$ ");
-			if (getline(&buffer, &buffer_size, stdin) == -1)
-			{
-				free(buffer);
-			       	exit(1);
-			}
-			
-
-			x = fork();
-			if (x == -1)
+			perror(argv[0]);
+			exit(0);
+		}
+		if(x != 0)
+		{
+			w = wait(0);
+			if(w == -1)
 			{
 				perror(argv[0]);
-				free(buffer);
-				exit(1);
-			}
-			if(x != 0)
-			{
-				w = wait(0);
-				if(w == -1)
-				{
-					perror(argv[0]);
-					free(buffer);
-					exit(1);
-				}
-			}
-			else
-			{
-
-				token = strtok(buffer, " \n");
-				r = execve(token, argv, envp);
-				if( r == -1)
-				{
-					perror(argv[0]);
-					free(buffer);
-					exit(1);
-				}
-		
+				exit(0);
 			}
 		}
+		else
+		{
+
+			arguments = malloc(32 * sizeof(char *));
+			for (i = 0; i < 32; ++i)
+				arguments[i] = (char *)malloc(128);
+			i = 0;
+			arguments[i] = strtok(buffer, " \t\n");
+			while (arguments[i])
+			{
+				i++;
+				arguments[i] = strtok(0, " \t\n");
+			}
+			r = execve(arguments[0], arguments, envp);
+			if( r == -1)
+			{
+				perror(argv[0]);
+				exit(0);
+			}
+
+		}
+	}
 	free(buffer);
 	return(0);
 }
