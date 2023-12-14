66c80d64224b5a9eab497a726b824277c4c181f4
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-12-14 05:44:27 +0100

    revert to back commit

--- simple_shell.c
+++ simple_shell.c
@@ -13,12 +13,13 @@ int main (__attribute__((unused)) int argc, __attribute__((unused)) char **argv,
 	while(1)
 	{
 		arguments = malloc(32 * sizeof(char *));
-		if(arguments == 0)
-			exit(0);
 		if(isatty(0))
 			write(1, "$ ", 2);
 		if (getline(&buffer, &buffer_size, stdin) == -1)
 		{
+			free(arguments);
+			free(env_var_line);
+			free(buffer);
 			exit(0);
 		}
 		/*for (i = 0; i < 32; i++)
@@ -44,8 +45,6 @@ int main (__attribute__((unused)) int argc, __attribute__((unused)) char **argv,
 			continue;
 		}
 		buffer_error = malloc(1024);
-		if(buffer_error == 0)
-			exit(0);
 		handle_path_expetions(&buffer_error, PATH, arguments[0], argv[0]);
 		free(buffer_error);
 		command = handle_path(arguments[0], PATH, argv[0]);
@@ -125,14 +124,14 @@ int *_strconcat(char **buffer, char *s1, char *s2)
 	}
 	(*buffer)[j] = '\0';
 
-	return 0;
+return 0;
 }
 
 
 void handle_path_expetions(char **buffer_error, char *PATH, char* command, char* shellname)
 {
 	int len;
-	_strconcat(buffer_error, shellname, ": 1: ");
+       	_strconcat(buffer_error, shellname, ": 1: ");
 	_strconcat(buffer_error, *buffer_error, command);
 	_strconcat(buffer_error, *buffer_error, ": not found\n");
 	len = _strlen(*buffer_error);
@@ -158,13 +157,11 @@ char *handle_path(char *command, char *PATH, char* shellname)
 	int i = 0, existence;
 	char *token, *path_to_check, *PATH_copied;
 	int x, y;
-	existence = access(command, F_OK);
-	if (existence == 0)
+	existence= access(command, F_OK);
+	if(existence == 0)
 		return command;
 	PATH_copied = malloc(1024);
-	if (PATH_copied == 0)
-		exit(0);
-	while (PATH[i] != 0)
+	while(PATH[i] != 0)
 	{
 		PATH_copied[i] = PATH[i];
 		i++;
@@ -173,34 +170,34 @@ char *handle_path(char *command, char *PATH, char* shellname)
 	token = strtok(PATH_copied, ":");
 	while(token)
 	{
-
-		x=0;
-		y=0;
-		path_to_check = malloc(sizeof(char)*512);
-		if(path_to_check == 0)
-		{
-			perror(shellname);
-			free(PATH_copied);
-			free(path_to_check); /*unsure*/
-			exit(31);
-		}
-		while(token[x] != '\0')
-		{
-			path_to_check[y] = token[x];
-			x++;
-			y++;
-		}
-		path_to_check[y] = 47;
+		
+	x=0;
+       	y=0;
+	path_to_check = malloc(sizeof(char)*512);
+	if(path_to_check == 0)
+	{
+		perror(shellname);
+		free(PATH_copied);
+		free(path_to_check); /*unsure*/
+		exit(31);
+	}
+	while(token[x] != '\0')
+	{
+		path_to_check[y] = token[x];
+		x++;
 		y++;
-		x = 0;
-		while(command[x] != '\0')
-		{
-			path_to_check[y] = command[x];
-			x++;
-			y++;
-		}
-		path_to_check[y] = '\0'; 
-
+	}
+	path_to_check[y] = 47;
+	y++;
+	x = 0;
+	while(command[x] != '\0')
+	{
+		path_to_check[y] = command[x];
+		x++;
+		y++;
+	}
+	path_to_check[y] = '\0'; 
+		
 		existence = access(path_to_check, F_OK);
 		if(existence == 0)
 		{
@@ -217,29 +214,29 @@ char *handle_path(char *command, char *PATH, char* shellname)
 	return 0;
 }
 /*char *path_concatenate(char *token, char* command)
-  {
-  int x=0, j=0;
-  char *temp = malloc(sizeof(char)*512);
-  while(token[x] != '\0')
-  {
-  temp[y] = token[x];
-  x++;
-  y++;
-  }
-  temp[y] = 47;
-  y++;
-  x = 0;
-  while(command[x] != '\0')
-  {
-  temp[y] = command[x];
-  x++;
-  y++;
-  }
-  temp[y] = '\0';
-  return temp;
+{
+	int x=0, j=0;
+	char *temp = malloc(sizeof(char)*512);
+	while(token[x] != '\0')
+	{
+		temp[y] = token[x];
+		x++;
+		y++;
+	}
+	temp[y] = 47;
+	y++;
+	x = 0;
+	while(command[x] != '\0')
+	{
+		temp[y] = command[x];
+		x++;
+		y++;
+	}
+	temp[y] = '\0';
+	return temp;
 
 
-  }*/
+}*/
 
 char *_getenv(char **env_var_line, char *name)
 {
@@ -248,15 +245,13 @@ char *_getenv(char **env_var_line, char *name)
 	char *token;
 	int j = 0;
 
-	while (environ[i])
+	while(environ[i])
 	{
 		/*temp = duplicate_string(environ[i]);*/
 		j = 0;
-		(*env_var_line) = malloc(sizeof(char) * 10000);
-		if ((*env_var_line) == 0)
-			exit(0);
+		(*env_var_line) = malloc(sizeof(char) * 6000);
 
-		while (environ[i][j] != '\0')
+		while(environ[i][j] != '\0')
 		{
 			(*env_var_line)[j] = environ[i][j];
 			j++;
@@ -264,7 +259,7 @@ char *_getenv(char **env_var_line, char *name)
 		(*env_var_line)[j] = '\0';
 
 		token = strtok(*env_var_line, "=");
-		if (_strcmp(token, name) == 0)
+		if(_strcmp(token, name) == 0)
 		{
 			token = strtok(0, "=");
 			return token;
